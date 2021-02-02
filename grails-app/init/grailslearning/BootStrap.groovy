0dd2812451ff80ff1df51d4e7f51ef3f56e2145c
Luis Villalobos <luisevm24@outlook.com>
2021-02-02 02:08:00 -0600

    init config to test logging

--- grails-app/init/grailslearning/BootStrap.groovy
+++ grails-app/init/grailslearning/BootStrap.groovy
@@ -3,6 +3,10 @@ package grailslearning
 class BootStrap {
 
     def init = { servletContext ->
+        log.debug "This is a debug message"
+        log.info "This is an info message"
+        log.warn "This is a warn message"
+        log.error "This is an error message"
     }
     def destroy = {
     }
