8c0443f34ddaffbdd3e16013a8410ad505553e81
Luis Villalobos <luisevm24@outlook.com>
2021-02-02 02:08:48 -0600

    loggin to console and simple file history

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,3 +1,4 @@
+import ch.qos.logback.core.util.FileSize
 import grails.util.BuildSettings
 import grails.util.Environment
 import org.springframework.boot.logging.logback.ColorConverter
@@ -9,6 +10,7 @@ conversionRule 'clr', ColorConverter
 conversionRule 'wex', WhitespaceThrowableProxyConverter
 
 // See http://logback.qos.ch/manual/groovy.html for details on configuration
+/*
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         charset = Charset.forName('UTF-8')
@@ -25,7 +27,7 @@ appender('STDOUT', ConsoleAppender) {
 def targetDir = BuildSettings.TARGET_DIR
 if (Environment.isDevelopmentMode() && targetDir != null) {
     appender("FULL_STACKTRACE", FileAppender) {
-        file = "${targetDir}/stacktrace.log"
+        file = "logs/stacktrace.log"
         append = true
         encoder(PatternLayoutEncoder) {
             pattern = "%level %logger - %msg%n"
@@ -33,4 +35,81 @@ if (Environment.isDevelopmentMode() && targetDir != null) {
     }
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
 }
-root(ERROR, ['STDOUT'])
+*/
+
+// root(ERROR, ['STDOUT'])
+
+jmxConfigurator('GrailsLearning')
+jmxConfigurator('GrailsLearning:type=ch.qos.logback.classic.jmx.JMXConfigurator')
+
+def timeOutput = timestamp("yyyy-MM-dd HH:mm:ss.SSS")
+def timeFile = timestamp('yyyy-MM')
+def utf8 = Charset.forName('UTF-8')
+def appends = []
+def logsFolder = ''
+
+if (Environment.isDevelopmentMode()) {
+    statusListener(OnConsoleStatusListener)
+    logsFolder = 'logs'
+    appends.add('CONSOLE')
+    appends.add('FILES')
+} else {
+    logsFolder = '/opt/grailslearning/logs'
+    appends.add('FILES_ROLLING')
+}
+
+println appends
+
+if (appends.contains('CONSOLE')) {
+    println 'console'
+    appender('CONSOLE', ConsoleAppender) {
+        encoder(PatternLayoutEncoder) {
+            charset = utf8
+            pattern = "%clr(%d{${timeOutput}}){magenta} " + // Date
+                    '[%clr(%p)] ' + // Log level
+                    '%clr(%logger){cyan}: ' + // Logger
+                    "\n\t%m%n%wex" // Message
+        }
+    }
+}
+
+if (appends.contains('FILES')) {
+    println 'files'
+    appender("FILES", RollingFileAppender) {
+        encoder(PatternLayoutEncoder) {
+            pattern = "%d{${timeOutput}} [%thread] %-5level %logger{40} - %msg%n"
+        }
+
+        rollingPolicy(TimeBasedRollingPolicy) {
+            FileNamePattern = "${logsFolder}/log-%d{yyyy-MM-dd}.log"
+            maxHistory = 30
+            totalSizeCap = FileSize.valueOf('100MB')
+        }
+    }
+}
+
+/*
+if (appends.contains('FILES_ROLLING')) {
+    appender("FILES_ROLLING", FileAppender) {
+
+        encoder(PatternLayoutEncoder) {
+            pattern = "%d{${timeOutput}} [%thread] %-5level %logger{40} - %msg%n"
+        }
+        rollingPolicy(SizeAndTimeBasedRollingPolicy) {
+            fileNamePattern = "${logsFolder}/logs-%d${timeFile}}.%i.log"
+            maxFileSize = '100mb'//FileSize.valueOf('100mb')
+            maxHistory = 12
+            totalSizeCap = '2gb'//FileSize.valueOf('2gb')
+        }
+    }
+}
+*/
+
+logger('grailslearning', DEBUG, appends)
+
+// https://docs.grails.org/latest/guide/conf.html
+// http://logback.qos.ch/manual/groovy.html
+// https://dennis-xlc.gitbooks.io/the-logback-manual/content/en/chapter-4-appenders/logback-core/rollingfileappender.html
+
+// https://tridion.stackexchange.com/questions/253/logback-xml-limit-the-size-of-files
+// https://lankydan.dev/2019/01/09/configuring-logback-with-spring-boot
