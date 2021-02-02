5cb89cf50236806a0f3d20dd2ac0f973127630cf
Luis Villalobos <luisevm24@outlook.com>
2021-02-02 08:28:46 -0600

    configuration of logs with time, size and envioroments

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -1,5 +1,4 @@
 import ch.qos.logback.core.util.FileSize
-import grails.util.BuildSettings
 import grails.util.Environment
 import org.springframework.boot.logging.logback.ColorConverter
 import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter
@@ -9,41 +8,11 @@ import java.nio.charset.Charset
 conversionRule 'clr', ColorConverter
 conversionRule 'wex', WhitespaceThrowableProxyConverter
 
-// See http://logback.qos.ch/manual/groovy.html for details on configuration
-/*
-appender('STDOUT', ConsoleAppender) {
-    encoder(PatternLayoutEncoder) {
-        charset = Charset.forName('UTF-8')
-
-        pattern =
-                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
-                        '%clr(%5p) ' + // Log level
-                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
-                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
-                        '%m%n%wex' // Message
-    }
-}
-
-def targetDir = BuildSettings.TARGET_DIR
-if (Environment.isDevelopmentMode() && targetDir != null) {
-    appender("FULL_STACKTRACE", FileAppender) {
-        file = "logs/stacktrace.log"
-        append = true
-        encoder(PatternLayoutEncoder) {
-            pattern = "%level %logger - %msg%n"
-        }
-    }
-    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
-}
-*/
-
-// root(ERROR, ['STDOUT'])
-
 jmxConfigurator('GrailsLearning')
 jmxConfigurator('GrailsLearning:type=ch.qos.logback.classic.jmx.JMXConfigurator')
 
 def timeOutput = timestamp("yyyy-MM-dd HH:mm:ss.SSS")
-def timeFile = timestamp('yyyy-MM')
+def timeFile = 'yyyy-MM-dd'
 def utf8 = Charset.forName('UTF-8')
 def appends = []
 def logsFolder = ''
@@ -58,10 +27,7 @@ if (Environment.isDevelopmentMode()) {
     appends.add('FILES_ROLLING')
 }
 
-println appends
-
 if (appends.contains('CONSOLE')) {
-    println 'console'
     appender('CONSOLE', ConsoleAppender) {
         encoder(PatternLayoutEncoder) {
             charset = utf8
@@ -74,41 +40,42 @@ if (appends.contains('CONSOLE')) {
 }
 
 if (appends.contains('FILES')) {
-    println 'files'
     appender("FILES", RollingFileAppender) {
         encoder(PatternLayoutEncoder) {
             pattern = "%d{${timeOutput}} [%thread] %-5level %logger{40} - %msg%n"
         }
 
-        rollingPolicy(TimeBasedRollingPolicy) {
-            FileNamePattern = "${logsFolder}/log-%d{yyyy-MM-dd}.log"
-            maxHistory = 30
+        rollingPolicy(SizeAndTimeBasedRollingPolicy) {
+            fileNamePattern = "$logsFolder/log-%d{${timeFile}}.%i.log"
+            maxFileSize = FileSize.valueOf('10MB')
+            maxHistory = 12
             totalSizeCap = FileSize.valueOf('100MB')
         }
     }
 }
 
-/*
 if (appends.contains('FILES_ROLLING')) {
-    appender("FILES_ROLLING", FileAppender) {
-
+    appender("FILES_ROLLING", RollingFileAppender) {
         encoder(PatternLayoutEncoder) {
             pattern = "%d{${timeOutput}} [%thread] %-5level %logger{40} - %msg%n"
         }
+
         rollingPolicy(SizeAndTimeBasedRollingPolicy) {
-            fileNamePattern = "${logsFolder}/logs-%d${timeFile}}.%i.log"
-            maxFileSize = '100mb'//FileSize.valueOf('100mb')
-            maxHistory = 12
-            totalSizeCap = '2gb'//FileSize.valueOf('2gb')
+            fileNamePattern = "${logsFolder}/log-%d{${timeFile}}.%i.log"
+            maxFileSize = FileSize.valueOf('100MB')
+            maxHistory = 30
+            totalSizeCap = FileSize.valueOf('1GB')
         }
     }
 }
-*/
 
-logger('grailslearning', DEBUG, appends)
+
+root(INFO, appends)
+// logger('grailslearning', DEBUG, appends)
 
 // https://docs.grails.org/latest/guide/conf.html
 // http://logback.qos.ch/manual/groovy.html
+// https://logback.qos.ch/manual/appenders.html
 // https://dennis-xlc.gitbooks.io/the-logback-manual/content/en/chapter-4-appenders/logback-core/rollingfileappender.html
 
 // https://tridion.stackexchange.com/questions/253/logback-xml-limit-the-size-of-files
