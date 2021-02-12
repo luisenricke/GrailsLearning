fce2366fa2744ac017db2eafb2fde4b7c13bc8fc
Luis Villalobos <luisevm24@outlook.com>
2021-02-11 23:59:02 -0600

    configuration of spock reports

---
+++ src/integration-test/resources/SpockConfig.groovy
@@ -0,0 +1,7 @@
+spockReports {
+    set([
+            'com.athaydes.spockframework.report.outputDir'              : 'reports/spock',
+            'com.athaydes.spockframework.report.aggregatedJsonReportDir': 'reports/spock',
+            'com.athaydes.spockframework.report.projectVersion'         : '1.0'
+    ])
+}
